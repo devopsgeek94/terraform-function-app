b2e6d5d7f957fdb4a2ac5dc02d3fb16c3e82ab1e
shubhamarora94 <8717073+shubhamarora94@users.noreply.github.com>
2023-08-22 22:21:19 +0530

    Update functionapp.tf

--- functionapp.tf
+++ functionapp.tf
@@ -3,12 +3,12 @@ module "function-app" {
   version = "7.5.1"
   # insert the 8 required variables here
 
-  client_name= ""
+  client_name= "test"
   environment= "dev"
   location= "westeu"
-  location_short= ""
-  logs_destinations_ids= ""
-  os_type= ""
-  resource_group_name= ""
-  stack= ""
+  location_short= "westeu"
+  logs_destinations_ids= "123"
+  os_type= "linux"
+  resource_group_name= "rg-functionapp"
+  stack= "java"
 }
