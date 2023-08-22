9df051bc24b3b2ff7a8567ccad9e0f70bee669d8
shubhamarora94 <8717073+shubhamarora94@users.noreply.github.com>
2023-08-22 22:16:38 +0530

    Create functionapp.tf

---
+++ functionapp.tf
@@ -0,0 +1,14 @@
+module "function-app" {
+  source  = "claranet/function-app/azurerm"
+  version = "7.5.1"
+  # insert the 8 required variables here
+
+  client_name= 
+  environment= "dev"
+  location= "westeu"
+  location_short= ""
+  logs_destinations_ids= ""
+  os_type= ""
+  resource_group_name= ""
+  stack= ""
+}
